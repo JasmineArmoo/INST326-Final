75fe23fadfa25922e19c48a3c2c42adc6461a4b3
lnchako <lnchako@terpmail.umd.edu>
2024-10-30 01:12:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ f1, f2, f3 = fruit
 print(f3)
 
 my_fruit = ["apple","grape","banana"]
-print (my_fruit [1:3])
+print(my_fruit [1:3])
 
 print(f"The entire fruit tuple: {fruit}")
 print(f"The entire my_list: {my_list}")
